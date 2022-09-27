a718b7c7bc10e20aa79dd374064cf868537d1d20
huntingh <huntingh@purdue.edu>
2022-09-27 15:14:39 -0400

    All LEDS included

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,30 +1,49 @@
-const int led1 = 2; //what pin the LED is
-const int button1 = 3;
+//Global Variable Definitions
+//Pin Definitions
+const int fr = 2; //Fingerprint Red
+const int fg = 3; //Fingerprint Green
+const int rr = 4; //RFID Red
+const int rg = 5; //RFID Green
+const int br = 6; //Battery Red
+const int by = 7; //Battery Yellow
+const int button1 = 8; //Reset Button
+
+//Other Variable Definitions
 int buttonState = 0;
 
+//Global Functions Definitions
+void turn_on_LED(int pin);
+void turn_off_LED(int pin);
+void read_button(int pin);
 
-void turn_on(int pin){
+void turn_on_LED(int pin){
   digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
 }
 
-void turn_off(int pin){
+void turn_off_LED(int pin){
   digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
 }
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(led1, OUTPUT);
+  pinMode(fr, OUTPUT);
+  pinMode(fg, OUTPUT);
+  pinMode(rr, OUTPUT);
+  pinMode(rg, OUTPUT);
+  pinMode(br, OUTPUT);
+  pinMode(by, OUTPUT);
   pinMode(button1, INPUT);
 }
 
-// the loop function runs over and over again forever
+void read_button(int pin){
+  buttonState = digitalRead(pin);
+}
+
 void loop() {
-  buttonState = digitalRead(button1);
-
-  if (buttonState == HIGH) {
-    turn_on(led1);
-  } else {
-    turn_off(led1);
-  }
-
+  read_button(button1);
+  if(buttonState == 1){
+    turn_on_LED(fr);
+    }
+  else{
+    turn_off_LED(fr);
+    }
 }
