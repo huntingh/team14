0356692bcd32a9f93507c6385425985b226697fc
Leo Seo <dongmin9710@gmail.com>
2022-10-04 15:29:41 -0400

    RFID scanning and LED works

--- rc522_simple/rc522_simple.ino
+++ rc522_simple/rc522_simple.ino
@@ -1,29 +1,57 @@
+// Code for RFID scanner & LED 
+
 #include <SPI.h>
-#include <RFID.h>
+#include <MFRC522.h>
 
 #define SDA_DIO 53
 #define RESET_DIO 49
 
-RFID RC522(SDA_DIO, RESET_DIO);
+MFRC522 mfrc522(SDA_DIO, RESET_DIO);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin();
-  RC522.init();
+  mfrc522.PCD_Init();
+  pinMode(13, OUTPUT); // Green LED
+  pinMode(12, OUTPUT); // Red LED
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (RC522.isCard()){
-    RC522.readCardSerial();
-    Serial.println("Card detected: ")
-    for (int i=0; i<5; i++) {
-      Serial.print(RC522.serNum[i], DEC)
-    }
-    Serial.println();
+  if ( !mfrc522.PICC_IsNewCardPresent()) {
+    return;
   }
-  delay(1000);
+  if ( !mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  Serial.print("Input RFID Tag Serial Number: ");
+  String content= "";
+  byte letter;
+  for (byte i =0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ?  " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ?  " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message: ");
+  content.toUpperCase();
+  if (content.substring(1) == "13 BD 81 1F") {
+    Serial.println("Authorized access");
+    Serial.println();
+    digitalWrite(13, HIGH);
+    delay(3000);
+    digitalWrite(13, LOW);    
+  } else {
+    Serial.println("Authorized denied");
+    Serial.println();
+    digitalWrite(12, HIGH);
+    delay(3000);
+    digitalWrite(12, LOW);       
+  }
+
 
 }
