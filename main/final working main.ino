ded7e4bb321c4aed97267f0d758254886ec4c33e
zombie3639 <70180259+zombie3639@users.noreply.github.com>
2022-12-06 15:07:54 -0500

    Add files via upload

---
+++ main/final working main.ino
@@ -0,0 +1,475 @@
+//Imports
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+//Global Variable Definitions
+#define SDA_DIO 53
+#define RESET_DIO 49
+#define RX_PIN 10 //RX IN from sensor (Green)
+#define TX_PIN 9 //TX OUT from Arduino (White)
+
+//Pin Definitions
+const int fr = 3; //Fingerprint Red
+const int fg = 4; //Fingerprint Green
+const int rr = 5; //RFID Red
+const int rg = 6; //RFID Green
+const int by = 7; //Battery Yellow
+const int br = 8; //Battery Red
+const int button1 = 2; //Reset Button
+const int lock = 11;
+const int mag = 48;
+uint8_t id;
+bool flag = true;
+
+//Other Variable Definitions
+int buttonState = 0;
+int exit_call = 0;
+int magState = 0;
+MFRC522 mfrc522(SDA_DIO, RESET_DIO);
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+// uint8_t id = 0;
+float voltage = 0;
+int battery_time = 0;
+
+//Global Functions Definitions
+void turn_on_LED(int pin);
+void turn_off_LED(int pin);
+void read_button(int pin);
+uint8_t readnumber(void);
+uint8_t getFingerprintID(void);
+void batteryStatusLED(void);
+void findVoltage(void);
+int scan_for_card(void);
+int validate_card_serial(void);
+void openLock(void);
+void closeLock(void);
+uint8_t getFingerprintEnroll(void);
+
+void turn_on_LED(int pin){
+  digitalWrite(pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+}
+
+void turn_off_LED(int pin){
+  digitalWrite(pin, LOW);    // turn the LED off by making the voltage LOW
+}
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+              }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+  p = finger.fingerSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+    //finger.LEDcontrol(FINGERPRINT_LED_ON, 0, FINGERPRINT_LED_RED);
+    //finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 10);
+    //should add part to open lock 
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    turn_on_LED(fr);
+    delay(3000);
+    turn_off_LED(fr); 
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID);
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  turn_on_LED(fg);
+  delay(3000);
+  turn_off_LED(fg);
+
+  openLock();
+  int locktimer = 0;
+  magState = digitalRead(mag);
+ 
+  while(magState == 0){
+    if(locktimer > 30){
+      break;
+    }
+    delay(1000);
+    magState = digitalRead(mag);
+    locktimer = locktimer+1;
+  }
+  while(magState == 1 && locktimer <= 30){//0 is closed, 1 open
+    read_button(button1);
+    if(buttonState == 1){
+      while (!getFingerprintEnroll());
+    }
+    delay(1000);
+    magState = digitalRead(mag); 
+  }
+  delay(1000);
+  closeLock();
+  delay(1000);
+  exit_call = 1;
+  return;
+}
+
+void batteryStatusLED(){
+  if(voltage<=1.6){ //2.75
+    turn_off_LED(by);
+    turn_on_LED(br);
+    //delay(1000);
+  }else if(voltage <= 1.9){ //3.75
+    turn_off_LED(br);
+    turn_on_LED(by);
+    //delay(1000);
+  }
+  else{
+    turn_off_LED(by);
+    turn_off_LED(br);
+    //delay(1000);
+  }
+}
+void findVoltage()
+{
+  float Total = 0;
+  float average_value = 0;
+  float total_value = 0;
+
+  int sensorValue = analogRead(A15); //read the A0 pin value
+  voltage = sensorValue * (5 / 1023.00); //convert the value to a true voltage.
+}
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  pinMode(fr, OUTPUT);
+  pinMode(fg, OUTPUT);
+  pinMode(rr, OUTPUT);
+  pinMode(rg, OUTPUT);
+  pinMode(lock, OUTPUT);
+  pinMode(mag, INPUT_PULLUP);
+  pinMode(br, OUTPUT);
+  pinMode(by, OUTPUT);
+  pinMode(button1, INPUT);
+  while(!Serial)
+  delay(100);
+  Serial.println("\n\nFingerprint Scanner Finger detect test");
+  finger.begin(57600);
+  delay(5);
+  if(finger.verifyPassword()){
+    Serial.println("Found fingerprint sensor!");
+  }
+  else {
+    Serial.println("Did not find fingerprint sensor :");
+    while(1) {
+      delay(1);
+    }
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+}
+}
+
+void read_button(int pin){
+  buttonState = digitalRead(pin);
+}
+
+int scan_for_card(){
+  if ( !mfrc522.PICC_IsNewCardPresent()) { //If a card isn't present, return 0
+  Serial.println("Card not present");
+    return 0;
+  }
+  if ( !mfrc522.PICC_ReadCardSerial()) { //If it can't read the card, return 0
+    return 0;
+  }  
+  Serial.println("Card present");
+  return 1; //This only happens when it passes both if's
+}
+int validate_card_serial(){
+  Serial.print("Input RFID Tag Serial Number: ");
+  String content= "";
+  byte letter;
+  for (byte i =0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ?  " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ?  " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message: ");
+  content.toUpperCase();
+  if (content.substring(1) == "13 BD 81 1F") {
+    Serial.println("Authorized access");
+    Serial.println();
+    return 1;
+    
+  } else {
+    Serial.println("Authorized denied");
+    Serial.println();
+    return 0;
+  }
+
+}
+void openLock(){
+  digitalWrite(lock,HIGH);
+}
+void closeLock(){
+  digitalWrite(lock,LOW);    
+}
+
+void loop() {
+  if(battery_time >= 3600){
+    findVoltage();
+    batteryStatusLED();
+    battery_time = 0;
+  }
+  
+  if(scan_for_card()){//If I found a card, I will search for the serial. If not, the loop restarts. 
+    if(!validate_card_serial()){ //If we can't read the serial
+      turn_on_LED(rr);
+      delay(3000);
+      battery_time = battery_time + 3;
+      turn_off_LED(rr); 
+      return; //Start void loop over
+    }
+  }
+  else{
+    delay(1000); //Wait 1 of a second
+    battery_time = battery_time + 1;
+    return; //Start void loop over
+  }
+  int validated = validate_card_serial();
+  if(validated){
+    turn_on_LED(rg);
+    delay(3000);
+    battery_time = battery_time + 3;
+    turn_off_LED(rg); 
+    long int t1 = millis();
+    long int time = 0;
+    while(time <= 10000) {
+      Serial.println(time);
+      Serial.println();  
+      getFingerprintID(); 
+      if(exit_call == 1){
+        exit_call = 0;
+        return;
+      }
+      time = millis() - t1;
+      
+    }
+    
+    if(time>=10000){
+      turn_on_LED(fr);
+      delay(3000);
+      battery_time = battery_time + 3;
+      turn_off_LED(fr);
+    }
+    battery_time = battery_time + time/1000;
+  }
+  else{
+    turn_on_LED(rr);
+    delay(3000);
+    battery_time = battery_time + 3;
+    turn_off_LED(rr); 
+  }
+  return;
+}
+
+uint8_t getFingerprintEnroll() {
+  int p = -1;
+  Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  Serial.println("Remove finger");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+  Serial.print("ID "); Serial.println(id);
+  p = -1;
+  Serial.println("Place same finger again");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK converted!
+  Serial.print("Creating model for #");  Serial.println(id);
+
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  Serial.print("ID "); Serial.println(id);
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Stored!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not store in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }
+
+  return true;
+}
